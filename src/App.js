da6daf245805d94545ad99e84652ed24bcda6755
Sarahmarie D Hollenstein <sholle7@gmail.com>
2020-05-20 23:12:21 -0500

    :tada: Finished MVP still working on Stretch

--- src/App.js
+++ src/App.js
@@ -1,43 +1,72 @@
 //TODO: STEP 1 - Import the useState hook.
-import React from "react";
+import React, {useState} from "react";
 import BottomRow from "./BottomRow";
 import "./App.css";
 
 function App() {
-  //TODO: STEP 2 - Establish your applictaion's state with some useState hooks.  You'll need one for the home score and another for the away score.
+  //TODO: STEP 2 - Establish your application's state with some useState hooks.  You'll need one for the home score and another for the away score.
+
+
+const [homePoints, setHomePoints] = useState(0);
+const makeTouchDown = e => {
+  setHomePoints(homePoints + 7);
+};
+const makeFieldGoal = e => {
+  setHomePoints(homePoints + 3);
+};
+
+const [awayPoints, setAwayPoints] = useState(0);
+const awayTouchDown = e => {
+  setAwayPoints(awayPoints + 7);
+};
+const awayFieldGoal = e => {
+  setAwayPoints(awayPoints + 3);
+};
+
+
 
   return (
-    <div className="container">
-      <section className="scoreboard">
-        <div className="topRow">
-          <div className="home">
-            <h2 className="home__name">Lions</h2>
+		<div className="container">
+			<section className="scoreboard">
+				<div className="topRow">
+					<div className="home">
+						<h2 className="home__name">Lions</h2>
 
-            {/* TODO STEP 3 - We need to change the hardcoded values in these divs to accept dynamic values from our state. */}
-            <div className="home__score">32</div>
-          </div>
-          <div className="timer">00:03</div>
-          <div className="away">
-            <h2 className="away__name">Tigers</h2>
-            <div className="away__score">32</div>
-          </div>
-        </div>
-        <BottomRow />
-      </section>
-      <section className="buttons">
-        <div className="homeButtons">
-
-          {/* TODO STEP 4 - Now we need to attach our state setter functions to click listeners. */}
-          <button className="homeButtons__touchdown">Home Touchdown</button>
-          <button className="homeButtons__fieldGoal">Home Field Goal</button>
-        </div>
-        <div className="awayButtons">
-          <button className="awayButtons__touchdown">Away Touchdown</button>
-          <button className="awayButtons__fieldGoal">Away Field Goal</button>
-        </div>
-      </section>
-    </div>
-  );
+						{/* TODO STEP 3 - We need to change the hard-code values in these <div>'s to accept dynamic values from our state. */}
+						<div className="home__score">
+							{' '}
+							<span>{homePoints}</span>
+						</div>
+					</div>
+					<div className="timer">00:03</div>
+					<div className="away">
+						<h2 className="away__name">Tigers</h2>
+						<div className="away__score">{awayPoints}</div>
+					</div>
+				</div>
+				<BottomRow />
+			</section>
+			<section className="buttons">
+				<div className="homeButtons">
+					{/* TODO STEP 4 - Now we need to attach our state setter functions to click listeners. */}
+					<button className="homeButtons__touchdown" onClick={makeTouchDown}>
+						Home Touchdown
+					</button>
+					<button className="homeButtons__fieldGoal" onClick={makeFieldGoal}>
+						Home Field Goal
+					</button>
+				</div>
+				<div className="awayButtons">
+					<button className="awayButtons__touchdown" onClick={awayTouchDown}>
+						Away Touchdown
+					</button>
+					<button className="awayButtons__fieldGoal" onClick={awayFieldGoal}>
+						Away Field Goal
+					</button>
+				</div>
+			</section>
+		</div>
+	);
 }
 
 export default App;
